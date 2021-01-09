681580a7596203ab084b57f1a92592788bd66066
wontae <beegramin9@gmail.com>
2021-01-09 20:33:31 +0900

    네비게이션 (Link 태그)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,23 @@
 import React from "react";
 import {Link} from "react-router-dom"
-
+import "./Navigation.css";
 function Navigation() {
     return (
-    <div>
+    <div className="nav">
         <Link to="/Home">Home</Link>
+        {/* React 라우터에 의해 주어진 props를 이용해서
+        각각 Movie 카드를 클릭하면 Movie 디테일을 들고 다른 페이지로 갈 수 있도록 */}
         <Link to="About">About</Link>
+        {/* to를 object로 교체할 수 있다. */}
+        <Link to={{
+            pathname: "/movie-detail",
+            state: {
+                fromNavigation : true
+            }
+            /* /about 링크로 state Object(=props) 를 보낸다. 
+            이게 어떤거냐면 jQuery로 /about&id=1?bid=2019 이렇게 보내야 했던걸
+            숨겨서 보낼 수 있다는 말이다. */
+        }}></Link>
     </div>
     )
     /* 근데 a 태그를 쓰면 React 답지 않게 누를 때마다 새로고침이 되어버림 4
